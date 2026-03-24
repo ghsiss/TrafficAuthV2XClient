7804749c0f4a06a329a98a3cbd2bf1395da22bbf
Andrew Chin <andrew.chin@ghsiss.com>
2026-03-24 11:17:26 -0400

    Release 0.0.12

--- Package.swift
+++ Package.swift
@@ -18,13 +18,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "libEncoderLib",
-            url: "https://iss-trafficauth.s3.us-west-2.amazonaws.com/TrafficAuthV2XClientReleases/releases/0.0.11/libEncoderLib.xcframework.zip",
-            checksum: "3d444711471e869b7c878ebf383062b3ce3962f3e51ca6752e34462de0de3e3e"
+            url: "https://iss-trafficauth.s3.us-west-2.amazonaws.com/TrafficAuthV2XClientReleases/releases/0.0.12/libEncoderLib.xcframework.zip",
+            checksum: "aa11a32de58eb515768e15025fedc1974158b0ec9c3a3b614365b76d64dc2d02"
         ),
         .binaryTarget(
             name: "trafficauth_v2xclient_ios",
-            url: "https://iss-trafficauth.s3.us-west-2.amazonaws.com/TrafficAuthV2XClientReleases/releases/0.0.11/TrafficAuthV2XClient.xcframework.zip",
-            checksum: "18e9314e516b2cdd19db3e79534cfcfbc383439d1df9f76644079a7757fde8e0"
+            url: "https://iss-trafficauth.s3.us-west-2.amazonaws.com/TrafficAuthV2XClientReleases/releases/0.0.12/TrafficAuthV2XClient.xcframework.zip",
+            checksum: "d3510d00fd3d5e9353aab029fba97f6bbc8d38d0742ebc7784db0dc0e9de4ebb"
         ),
         .target(
             name: "TrafficAuthV2XClient",
